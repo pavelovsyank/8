54b722b6abceac2d893d6083bf8af1c0b7c4fe72
pavel <p@b.ru>
2024-01-24 22:33:16 +0200

    ggg

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+@app.get("/produkt/{item_id}")
+async def read_item(item_id):
+    return {"produkt_id": item_id}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 pip install "fastapi[all]"
 pip install "uvicorn[standard]"
 
+сделать гитинит проекта(например папка 8)
+сделать комит
+
+создать новый репозиторий на гитхаб(например 8)
+связать этот репозиторий с проектом:
+git remote add origin git@github.com:pavelovsyank/8.git
