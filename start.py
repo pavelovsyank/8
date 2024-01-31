e4c1d86636c2fce2615357a6f1180a90787585e0
pavel <p@b.ru>
2024-01-31 23:40:27 +0200

    add host

--- start.py
+++ start.py
@@ -3,4 +3,4 @@ from app.config import settings
 
 if __name__ == "__main__":
     #print(f"port = {settings.port}")
-    uvicorn.run("app.main:app", port=settings.port)
+    uvicorn.run("app.main:app", host="0.0.0.0" port=settings.port)
