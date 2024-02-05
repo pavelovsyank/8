93772315effa7fc69f98e86c9ab6885f7fccd043
pavel <p@b.ru>
2024-02-06 00:27:45 +0200

    add str

--- app/main.py
+++ app/main.py
@@ -69,6 +69,82 @@ async def potato(request: Request, id: str):
     return templates.TemplateResponse(
         request=request, name="potato.html", context={"id": id}
     )
+@app.get("/apricot/{id}", response_class=HTMLResponse)
+async def apricot(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="apricot.html", context={"id": id}
+    )
+@app.get("/pineapple/{id}", response_class=HTMLResponse)
+async def pineapple(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="pineapple.html", context={"id": id}
+    )
+@app.get("/avocado/{id}", response_class=HTMLResponse)
+async def avocado(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="avocado.html", context={"id": id}
+    )
+@app.get("/banana/{id}", response_class=HTMLResponse)
+async def banana(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="banana.html", context={"id": id}
+    )
+@app.get("/kiwi/{id}", response_class=HTMLResponse)
+async def kiwi(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="kiwi.html", context={"id": id}
+    )
+@app.get("/lime/{id}", response_class=HTMLResponse)
+async def lime(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="lime.html", context={"id": id}
+    )
+@app.get("/lemon/{id}", response_class=HTMLResponse)
+async def lemon(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="lemon.html", context={"id": id}
+    )
+@app.get("/mango/{id}", response_class=HTMLResponse)
+async def mango(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="mango.html", context={"id": id}
+    )
+@app.get("/orange/{id}", response_class=HTMLResponse)
+async def orange(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="orange.html", context={"id": id}
+    )
+@app.get("/pomelo/{id}", response_class=HTMLResponse)
+async def pomelo(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="pomelo.html", context={"id": id}
+    )
+@app.get("/carrot/{id}", response_class=HTMLResponse)
+async def carrot(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="carrot.html", context={"id": id}
+    )
+@app.get("/pepper/{id}", response_class=HTMLResponse)
+async def pepper(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="pepper.html", context={"id": id}
+    )
+@app.get("/coconut/{id}", response_class=HTMLResponse)
+async def coconut(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="coconut.html", context={"id": id}
+    )
+@app.get("/broccoli/{id}", response_class=HTMLResponse)
+async def broccoli(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="broccoli.html", context={"id": id}
+    )
+@app.get("/corn/{id}", response_class=HTMLResponse)
+async def corn(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="corn.html", context={"id": id}
+    )
+
 
 
 
