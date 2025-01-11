b0a001a5c630fe8938c62f2b0852f62a15062e90
pavel <p@b.ru>
2025-01-11 11:35:40 +0200

    aa

--- app/main.py
+++ app/main.py
@@ -9,30 +9,46 @@ app = FastAPI()
 dir_static = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'static')
 
 dir_templates = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
-#print(f"dir static = {dir_static}")
-#print(f"dir templates = {dir_templates}")
+print(f"dir static = {dir_static}")
+print(f"dir templates = {dir_templates}")
 
 app.mount("/static", StaticFiles(directory=dir_static), name="static")
 
 
 templates = Jinja2Templates(directory=dir_templates)
 
-@app.get("/descr/{id}", response_class=HTMLResponse)
-async def descr(request: Request, id: str):
-    filename = f"{id}.html"
-    print(f"filename={filename}")
-    return templates.TemplateResponse(
-        request=request, name=filename
-    )
+# @app.get("/descr/{id}", response_class=HTMLResponse)
+# async def descr(request: Request, id: str):
+#     filename = f"{id}.html"
+#     print(f"filename={filename}")
+#     return templates.TemplateResponse(
+#         request=request, name=filename
+#     )
 
-@app.get("/two", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def two(request: Request):
     return templates.TemplateResponse(
-        request=request, name="2.html"
+        request=request, name="index.html"
     )
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="1.html"
-    )
+# @app.get("/")
+# async def root(request: Request):
+#     return templates.TemplateResponse(
+#         request=request, name="1.html"
+#     )
+
+@app.get("/json")
+async def api_json():
+    print("massage: Hello World ")
+    return {"message": "Hello World"}
+
+@app.get("/json2")
+async def api_json2():
+    print("massage: Hello World ")
+    return {"message": "Hello World"}
+
+@app.get("/json3")
+async def api_json3():
+    print("massage: Hello World ")
+    return {"message": "Hello World"}
+
